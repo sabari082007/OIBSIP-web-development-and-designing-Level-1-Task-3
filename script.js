bf125729cf704b31e5df5c52913044c20aae5522
sabari082007 <vdlogaramji28@gmail.com>
2026-06-11 07:56:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,65 @@
+let celsiusInput = document.querySelector('#celsius > input')
+let fahrenheitInput = document.querySelector('#fahrenheit > input')
+let kelvinInput = document.querySelector('#kelvin > input')
+ 
+let btn  = document.querySelector('.button button')
+
+function roundNumber(number){
+    return Math.round(number * 100) / 100
+}
+
+function isValidNumber(value){
+    return !Number.isNaN(value)
+}
+
+
+celsiusInput.addEventListener('input',function(){
+    let cTemp = parseFloat(celsiusInput.value)
+    if (!isValidNumber(cTemp)){
+        fahrenheitInput.value = ''
+        kelvinInput.value = ''
+        return
+    }
+
+    let fTemp = (cTemp * (9/5)) + 32
+    let kTemp = cTemp + 273.15
+
+    fahrenheitInput.value = roundNumber(fTemp)
+    kelvinInput.value = roundNumber(kTemp)
+})
+
+fahrenheitInput.addEventListener('input',function(){
+    let fTemp = parseFloat(fahrenheitInput.value)
+    if (!isValidNumber(fTemp)){
+        celsiusInput.value = ''
+        kelvinInput.value = ''
+        return
+    }
+
+    let cTemp = (fTemp - 32) * (5/9)
+    let kTemp = cTemp + 273.15
+
+    celsiusInput.value = roundNumber(cTemp)
+    kelvinInput.value = roundNumber(kTemp)
+})
+
+kelvinInput.addEventListener('input',function(){
+    let kTemp = parseFloat(kelvinInput.value)
+    if (!isValidNumber(kTemp)){
+        celsiusInput.value = ''
+        fahrenheitInput.value = ''
+        return
+    }
+
+    let fTemp = (kTemp - 273.15) * (9/5) + 32
+    let cTemp = kTemp - 273.15
+
+    celsiusInput.value = roundNumber(cTemp)
+    fahrenheitInput.value = roundNumber(fTemp)
+})
+
+btn.addEventListener('click',()=>{
+    celsiusInput.value=""
+    fahrenheitInput.value=""
+    kelvinInput.value=""
+})
